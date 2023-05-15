497f0842b9bd0bb23bb5864861890cc7e739a98b
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2023-05-15 15:43:45 -0300

    feat(tailwind): add initial colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "blue": "#002a87",
+        "light-violet": "#9340ff"
+      }
+    },
   },
   plugins: [],
 }
+
+  // linear-gradient(-135deg,#002a87 25%,#9340ff 100%)
