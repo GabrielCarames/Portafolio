7db61962360785118178517882390bd307bd0cf1
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2023-05-15 16:05:02 -0300

    feat(tailwind): add custom screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,15 @@ export default {
       colors: {
         "blue": "#002a87",
         "light-violet": "#9340ff"
-      }
+      },
+        screens: {
+      'xs': '500px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
     },
   },
   plugins: [],
