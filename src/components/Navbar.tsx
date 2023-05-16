9a5e379f207969fc1c441e4a8b7ac9708dfad154
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2023-05-16 17:23:52 -0300

    feat(navbar): add opacity animation on scroll

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,28 @@
+import { useEffect, useState } from "react"
+import "../assets/navbar.css"
+
 const Navbar = () => {
+  const [isNavbarFixed, setIsNavbarFixed] = useState(false)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const isFixed = window.scrollY > 200
+      setIsNavbarFixed(isFixed)
+    }
+    window.addEventListener("scroll", handleScroll)
+    return () => {
+      removeEventListener("scroll", handleScroll)
+    }
+  }, [])
+
   return (
-    <header className="hidden xs:block w-full h-16 text-white bg-[#ffffff0d] border-b border-b-[#ffffff1a] fixed top-0 left-0">
-      <nav className="h-full flex justify-center items-center">
+    <header className="hidden xs:block w-full h-16 text-white bg-[#ffffff0d] border-b border-b-[#ffffff1a] top-0 left-0 fixed z-50 navbar">
+      <div
+        className={`fixed-navbar ${
+          isNavbarFixed ? "fixed-navbar-active" : "fixed-navbar-inactive"
+        }`}
+      ></div>
+      <nav className="h-full flex justify-center items-center relative z-50">
         <ul className="flex gap-10 items-center h-full">
           <li className="w-24 h-full flex justify-center items-center hover:font-bold hover:border-b hover:border-b-white cursor-pointer duration-75">
             <span>Sobre mí</span>
