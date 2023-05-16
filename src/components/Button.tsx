88b82e8edd6e1fee60ea9c2ad51db38abc6d43ea
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2023-05-16 08:51:15 -0300

    feat: add "Button" component

---
+++ src/components/Button.tsx
@@ -0,0 +1,24 @@
+import { FC } from "react"
+import { ButtonProps, Variants } from "../interfaces"
+
+const variants: Variants = {
+  green: "bg-green border-[3px] border-light-green",
+  blue: "bg-gradient-to-r from-blue-2 to-blue-3 border-[3px] border-blue-4 hover:bg-none"
+}
+
+const defaultClassName =
+  "font-bold focus:outline-none hover:bg-white hover:text-gray-800 flex justify-center items-center text-sm md:text-base px-5 py-2 box-border duration-75 text-center rounded-3xl"
+
+const Button: FC<ButtonProps> = ({ variant, text, className, onClick, Icon, iconClassName }) => {
+  return (
+    <button
+      className={`${variants[variant as keyof Variants]} ${defaultClassName} ${className}`}
+      onClick={onClick}
+    >
+      {Icon ? <Icon className={`h-5 w-5 mr-2 ${iconClassName}`} /> : null}
+      {text}
+    </button>
+  )
+}
+
+export default Button
--- src/sections/AboutMe.tsx
+++ src/sections/AboutMe.tsx
@@ -1,19 +1,23 @@
 import type { FC } from "react"
 import selfie from "../../public/selfie.webp"
 import Button from "../components/Button"
-//  mb-52
+
 const AboutMe: FC = () => {
   return (
-    <section className="w-full h-full flex flex-col justify-center items-center">
-      <article className="w-full max-w-4xl md:max-w-5xl h-max flex flex-wrap-reverse md:flex-nowrap justify-center md:justify-between items-center gap-10 px-10">
-        <div className="w-full max-w-none md:max-w-lg text-center">
+    <section className="w-full h-full flex justify-center items-center">
+      <article className="w-full max-w-4xl md:max-w-5xl h-max flex flex-wrap-reverse md:flex-nowrap justify-center md:justify-between items-center gap-10 mb-40 px-10">
+        <div className="w-full max-w-none md:max-w-lg text-left">
           <h1 className="text-4xl md:text-5xl font-bold">Gabriel Caramés</h1>
           <h2 className="text-xl md:text-2xl mt-2">Desarrollador Front-end</h2>
           <p className="text-sm md:text-base mt-5 text-slate-100">
-            Experiencia trabajando para proyectos freelance. Enfoque orientado en el uso de buenas
-            prácticas de programación y orientado a resultados efectivos. Comprometido en el trabajo
-            en equipo y en la satisfacción de los clientes con el producto final.
+            Experiencia trabajando para proyectos freelance. Enfoque centrado en el uso de prácticas
+            de programación sólidas y orientado a lograr resultados efectivos. Comprometido en el
+            trabajo en equipo y en la satisfacción de los clientes con el producto final.
           </p>
+          <div className="w-full flex gap-2 mt-10">
+            <Button variant="green" text="Ver Curriculum" />
+            <Button variant="blue" text="Ver das" />
+          </div>
         </div>
         <div className="w-36 h-36 md:w-40 md:h-40 lg:w-72 lg:h-72 rounded-full shadow-2xl shadow-black">
           <img
@@ -23,9 +27,6 @@ const AboutMe: FC = () => {
           />
         </div>
       </article>
-      <div>
-        <Button variant="primary" text="Ver Curriculum" />
-      </div>
     </section>
   )
 }
