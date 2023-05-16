353fe11f77d5b88622b8f82fe7c6309b3eaafcc2
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2023-05-16 08:51:07 -0300

    feat: add interfaces

---
+++ src/interfaces.ts
@@ -0,0 +1,13 @@
+export type Variants = {
+  green: string
+  blue: string
+}
+
+export interface ButtonProps {
+  variant: "green" | "blue"
+  text: string
+  className?: string
+  Icon?: React.ForwardRefExoticComponent<React.SVGProps<SVGSVGElement>>
+  iconClassName?: string
+  onClick?: () => void
+}
