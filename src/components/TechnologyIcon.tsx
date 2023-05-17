6c3eef5cb9dbe0d888e182ab2064e75b964c3ed8
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2023-05-17 17:26:19 -0300

    feat: add Zustand technology

--- src/components/TechnologyIcon.tsx
+++ src/components/TechnologyIcon.tsx
@@ -1,6 +1,7 @@
 import TypeScriptIcon from "./icons/TypeScriptIcon"
 import JavaScriptIcon from "./icons/JavaScriptIcon"
 import TailwindIcon from "./icons/TailwindIcon"
+import ZustandIcon from "./icons/ZustandIcon"
 import GraphQLIcon from "./icons/GraphQLIcon"
 import ApolloIcon from "./icons/ApolloIcon"
 import SocketIcon from "./icons/SocketIcon"
@@ -8,7 +9,7 @@ import NextjsIcon from "./icons/NextjsIcon"
 import ReactIcon from "./icons/ReactIcon"
 import ReduxIcon from "./icons/ReduxIcon"
 import SassIcon from "./icons/SassIcon"
-import HTMLIcon from "./icons/HtmlIcon"
+import HTMLIcon from "./icons/HTMLIcon"
 import CSSIcon from "./icons/CSSIcon"
 import type { FC } from "react"
 
@@ -32,7 +33,8 @@ const TECHNOLOGIES_LIST: TechnologiesListProps = {
   CSS: <CSSIcon className={DEFAULT_CLASSNAME} />,
   Sass: <SassIcon className={DEFAULT_CLASSNAME} />,
   Redux: <ReduxIcon className={DEFAULT_CLASSNAME} />,
-  Socket: <SocketIcon className={DEFAULT_CLASSNAME} />,
+  Zustand: <ZustandIcon className={DEFAULT_CLASSNAME} />,
+  "Socket.io": <SocketIcon className={DEFAULT_CLASSNAME} />,
   "Apollo Client": <ApolloIcon className={DEFAULT_CLASSNAME} />,
   GraphQL: <GraphQLIcon className={DEFAULT_CLASSNAME} />
 }
--- src/sections/Technologies.tsx
+++ src/sections/Technologies.tsx
@@ -6,12 +6,12 @@ import type { FC } from "react"
 const Technologies: FC = () => {
   return (
     <section className="w-full h-screen flex justify-center">
-      <div className="w-full max-w-7xl h-max flex flex-col gap-10 mt-10">
+      <div className="w-full max-w-7xl h-max flex flex-col gap-20 mt-10">
         <Title text="Tecnologías" />
-        <ul className="flex flex-wrap justify-center gap-36 px-8">
+        <ul className="flex flex-wrap justify-center gap-20 px-8">
           {technologies?.map(technology => (
-            <li key={technology} className="flex flex-col gap-2 justify-center items-center">
-              <div className="w-16 h-16 lg:w-20 lg:h-20 filter drop-shadow-md">
+            <li key={technology} className="w-24 flex flex-col gap-2 justify-center items-center">
+              <div className="w-16 h-16 lg:w-20 lg:h-20 filter drop-shadow-xl">
                 <TechnologyIcon technology={technology} />
               </div>
               <p className="font-semibold text-gray-800 text-sm lg:text-base">{technology}</p>
