885d0aa5bff9e133b25916e63dc2df563d66ec6b
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2023-05-17 15:01:46 -0300

    feat(Button): add new "link" type

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -7,16 +7,23 @@ const variants: Variants = {
 }
 
 const defaultClassName =
-  "font-bold focus:outline-none hover:bg-white hover:text-gray-800 flex justify-center items-center text-sm lg:text-base px-5 py-2 box-border duration-75 text-center rounded-3xl"
+  "w-max flex gap-3 lg:gap-5 font-bold focus:outline-none text-white cursor-pointer hover:bg-white hover:text-gray-800 flex justify-center items-center text-sm lg:text-base px-3 py-[6px] lg:px-5 lg:py-2 box-border duration-75 text-center rounded-3xl"
 
-const Button: FC<ButtonProps> = ({ variant, text, className, onClick, Icon, iconClassName }) => {
-  return (
+const Button: FC<ButtonProps> = ({ variant, type, href, className, onClick, children }) => {
+  return type === "link" ? (
+    <a
+      className={`${variants[variant as keyof Variants]} ${defaultClassName} ${className}`}
+      href={href}
+      target="_blank"
+    >
+      {children}
+    </a>
+  ) : (
     <button
       className={`${variants[variant as keyof Variants]} ${defaultClassName} ${className}`}
       onClick={onClick}
     >
-      {Icon ? <Icon className={`h-5 w-5 mr-2 ${iconClassName}`} /> : null}
-      {text}
+      {children}
     </button>
   )
 }
