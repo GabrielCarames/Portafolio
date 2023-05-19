24e8e0904936c5c661034bf2efa6942b1bc95baa
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2023-05-19 11:08:00 -0300

    refactor(Project): add new size variant prop

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,38 +1,75 @@
-import type { FC } from "react"
-import type { ProjectProps } from "../interfaces"
-import TechnologiesTags from "./TechnologiesTags"
-import GithubIcon from "./icons/GithubIcon"
-import LinkIcon from "./icons/LinkIcon"
-import Button from "./Button"
+import TechnologiesTags from "./TechnologiesTags";
+import GithubIcon from "./icons/GithubIcon";
+import LinkIcon from "./icons/LinkIcon";
+import Button from "./Button";
+import type { FC } from "react";
+import type { ProjectProps } from "../interfaces";
 
-const Project: FC<ProjectProps> = ({ project }) => {
+const sizes = {
+  small: {
+    li: "max-w-[600px]",
+    article: "flex-col gap-5",
+    header: "",
+    div: "gap-5",
+  },
+  large: {
+    li: "",
+    article: "flex-wrap gap-5 lg:flex-nowrap",
+    header: "lg:w-3/4",
+    div: "lg:w-2/4",
+  },
+};
+
+const Project: FC<ProjectProps> = ({ project, size }) => {
   return (
-    <li className="w-full bg-white p-5 rounded-xl shadow-xl">
-      <article className="flex flex-wrap lg:flex-nowrap gap-5">
-        <header className="w-full lg:w-3/4 relative">
-          <a className="w-full block" href={project.links.deployment} target="_blank">
+    <li
+      className={`w-full rounded-xl bg-white p-5 shadow-xl ${sizes[size].li}`}
+    >
+      <article className={`flex ${sizes[size].article}`}>
+        <header className={`relative w-full ${sizes[size].header}`}>
+          <a
+            className="block w-full"
+            href={project.links.deployment}
+            target="_blank"
+          >
             <div className="overflow-hidden">
               <img
-                className="w-full object-cover transform-gpu hover:scale-105 duration-150"
+                className="w-full transform-gpu object-cover duration-150 hover:scale-105"
                 src={project.image}
                 alt={project.alt}
               />
             </div>
           </a>
         </header>
-        <div className="flex flex-col justify-between w-full lg:w-2/4">
-          <div className="flex flex-col gap-5 mb-10 xl:mb-0">
-            <h4 className="font-bold text-xl lg:text-2xl text-violet-3">{project.title}</h4>
-            <p className="text-gray-800 leading-5 text-sm lg:text-base">{project.description}</p>
+        <div
+          className={`flex w-full flex-col justify-between ${sizes[size].div}`}
+        >
+          <div className="mb-10 flex flex-col gap-5 xl:mb-0">
+            <h4 className="text-xl font-bold text-violet-3 lg:text-2xl">
+              {project.title}
+            </h4>
+            <p className="text-sm leading-5 text-gray-800 lg:text-base">
+              {project.description}
+            </p>
             <div className="flex flex-wrap gap-5">
-              <Button type="link" variant="green" className="w-24" href={project.links.repository}>
+              <Button
+                type="link"
+                variant="green"
+                className="w-24"
+                href={project.links.repository}
+              >
                 Ver repositorio
-                <GithubIcon className="!w-5 !h-5 fill-white" />
+                <GithubIcon className="!h-5 !w-5 fill-white" />
               </Button>
               {project.links.deployment && (
-                <Button type="link" variant="blue" className="w-24" href={project.links.deployment}>
+                <Button
+                  type="link"
+                  variant="blue"
+                  className="w-24"
+                  href={project.links.deployment}
+                >
                   Ver proyecto
-                  <LinkIcon className="!w-5 !h-5 fill-white" />
+                  <LinkIcon className="!h-5 !w-5 fill-white" />
                 </Button>
               )}
             </div>
@@ -41,7 +78,7 @@ const Project: FC<ProjectProps> = ({ project }) => {
         </div>
       </article>
     </li>
-  )
-}
+  );
+};
 
-export default Project
+export default Project;
